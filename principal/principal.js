daacd46e993ed8bc6eeb10bca0907b769c417929
andrey <andreyufu@gmail.com>
2026-07-01 21:48:54 -0300

    novobagulho

--- principal/principal.js
+++ principal/principal.js
@@ -14,3 +14,34 @@ const voltar = document.getElementById("btn-main")
             window.location.href = "../LOGIN/login.html"
         })
     }
+
+// SEPARAÇÃO DE GUIAS
+
+function chatao() {
+    document.getElementById("principal").style.display = "block";
+    document.getElementById("cronograma").style.display = "none";
+    document.getElementById("biblioteca").style.display = "none";
+    document.getElementById("progresso").style.display = "none";
+}
+
+function cronograma() {
+    document.getElementById("principal").style.display = "none";
+    document.getElementById("cronograma").style.display = "block";
+    document.getElementById("biblioteca").style.display = "none";
+    document.getElementById("progresso").style.display = "none";
+}
+
+function minhabiblioteca() {
+    document.getElementById("principal").style.display = "none";
+    document.getElementById("cronograma").style.display = "none";
+    document.getElementById("biblioteca").style.display = "block";
+    document.getElementById("progresso").style.display = "none";
+}
+
+function meuprogresso() {
+    document.getElementById("principal").style.display = "none";
+    document.getElementById("cronograma").style.display = "none";
+    document.getElementById("biblioteca").style.display = "none";
+    document.getElementById("progresso").style.display = "block";
+}
+
